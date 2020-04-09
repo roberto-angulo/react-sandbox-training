b8b875d4b6cb1c586d8db7a59a2b03c268f65f3c
Roberto Angulo <robertoeat91@gmail.com>
2020-04-09 15:32:50 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import ReactDOM from "react-dom";
+import React from "react";
+
+const Element = props => (
+  <div>
+    <h1>Titlte just here</h1>
+    <p>{props.description}</p>
+  </div>
+);
+
+const MainElement = WrappedComponent => {
+  return props => (
+    <main>
+      <WrappedComponent {...props} />
+    </main>
+  );
+};
+
+const MainElementComponent = MainElement(Element);
+
+ReactDOM.render(
+  <MainElementComponent description={"it that fine?"} />,
+  document.getElementById("root")
+);
